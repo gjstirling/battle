876eee729b28874989f260b4ebaccd7b54a9f088
eballantine <lizzy.kayballantine@gmail.com>
2021-08-09 16:54:06 +0100

    add named-cat with name provided by parameters in url

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/lizzy' do
   "LIZZY!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Graeme", "Lizzy", "Bella", "Scrabble"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
