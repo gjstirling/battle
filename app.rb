e22db6b97f3d30fcec22b873c270e400ff5c4643
eballantine <lizzy.kayballantine@gmail.com>
2021-08-09 15:42:40 +0100

    add more routes, style a cat

--- app.rb
+++ app.rb
@@ -1,2 +1,20 @@
 require 'sinatra' 
+require 'sinatra/reloader' if development?
 
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "PSST!"
+end
+
+get '/lizzy' do
+  "LIZZY!"
+end
+
+get '/cat' do
+  "<div style='border: 5px dotted green; padding:20px'>
+  <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
+end
